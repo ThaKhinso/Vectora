ba4471cb97c0f0020914e9f0db64d8a79fca0f82
ThaKhinSo <usersoe123@gmail.com>
2026-03-10 21:28:26 +0630

    properties

--- Vectora_Editor/Panels/SceneHirearchyPanel.cpp
+++ Vectora_Editor/Panels/SceneHirearchyPanel.cpp
@@ -3,6 +3,7 @@
 #include "Scene/Components.h"
 
 #include <imgui.h>
+#include <glm/gtc/type_ptr.hpp>
 
 namespace Vectora {
 	SceneHirearchyPanel::SceneHirearchyPanel(const Ref<Scene>& scene)
@@ -23,6 +24,16 @@ namespace Vectora {
 			Entity entity{ entityID, m_Context.get() };
 			DrawEntityNode(entity);
 		}
+
+		if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered())
+		{
+			m_SelectionContext = {};
+		}
+		ImGui::End();
+
+		ImGui::Begin("Properties");
+		if (m_SelectionContext)
+			DrawComponents(m_SelectionContext);
 		ImGui::End();
 	}
 	void SceneHirearchyPanel::DrawEntityNode(Entity entity)
@@ -43,6 +54,33 @@ namespace Vectora {
 			if (opened)
 				ImGui::TreePop();
 			ImGui::TreePop();
+		}
+	}
+
+	void SceneHirearchyPanel::DrawComponents(Entity entity)
+	{
+		if (entity.HasComponent<TagComponent>())
+		{
+			auto& tag = entity.GetComponent<TagComponent>().Tag;
+
+			char buffer[255];
+			memset(buffer, 0, sizeof(buffer));
+			strcpy_s(buffer, sizeof(buffer), tag.c_str());
+			if (ImGui::InputText("Tag", buffer, sizeof(buffer)))
+			{
+				tag = std::string(buffer);
+			}
+		}
+
+		if (entity.HasComponent<TransformComponent>())
+		{
+			if (ImGui::TreeNodeEx((void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen, "Transform"))
+			{
+				auto& transform = entity.GetComponent<TransformComponent>().Transform;
+				ImGui::DragFloat3("Position", glm::value_ptr(transform));
+
+				ImGui::TreePop();
+			}
 		}
 	}
 }
