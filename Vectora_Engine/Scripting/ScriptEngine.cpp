d3f01be651b8b68d4ded9ceea86c5beaffbd2fe4
soethiha <usersoe123@gmail.com>
2026-04-17 18:01:27 +0630

    started C# api

--- Vectora_Engine/Scripting/ScriptEngine.cpp
+++ Vectora_Engine/Scripting/ScriptEngine.cpp
@@ -1,117 +1,123 @@
 #include "vpch.h"
 #include "ScriptEngine.h"
-#include "Vectora.h"
-
+#include "ScriptGlue.h"
 
 #include "mono/jit/jit.h"
 #include "mono/metadata/assembly.h"
 
 namespace Vectora {
+	namespace Utils {
+		static char* ReadBytes(const std::filesystem::path& filepath, uint32_t* outSize)
+		{
+			std::ifstream stream(filepath, std::ios::binary | std::ios::ate);
 
+			if (!stream)
+			{
+				// Failed to open the file
+				return nullptr;
+			}
+
+			std::streampos end = stream.tellg();
+			stream.seekg(0, std::ios::beg);
+			uint32_t size = end - stream.tellg();
+
+			if (size == 0)
+			{
+				// File is empty
+				return nullptr;
+			}
+
+			char* buffer = new char[size];
+			stream.read((char*)buffer, size);
+			stream.close();
+
+			*outSize = size;
+			return buffer;
+		}
+
+		static MonoAssembly* LoadMonoAssembly(const std::filesystem::path& assemblyPath)
+		{
+			uint32_t fileSize = 0;
+			char* fileData = ReadBytes(assemblyPath, &fileSize);
+
+			// NOTE: We can't use this image for anything other than loading the assembly because this image doesn't have a reference to the assembly
+			MonoImageOpenStatus status;
+			MonoImage* image = mono_image_open_from_data_full(fileData, fileSize, 1, &status, 0);
+
+			if (status != MONO_IMAGE_OK)
+			{
+				const char* errorMessage = mono_image_strerror(status);
+				// Log some error message using the errorMessage data
+				return nullptr;
+			}
+
+			std::string tempPath = assemblyPath.string();
+			MonoAssembly* assembly = mono_assembly_load_from_full(image, tempPath.c_str(), &status, 0);
+			mono_image_close(image);
+
+			// Don't forget to free the file data
+			delete[] fileData;
+
+			return assembly;
+		}
+		void PrintAssemblyTypes(MonoAssembly* assembly)
+		{
+			MonoImage* image = mono_assembly_get_image(assembly);
+			const MonoTableInfo* typeDefinitionsTable = mono_image_get_table_info(image, MONO_TABLE_TYPEDEF);
+			int32_t numTypes = mono_table_info_get_rows(typeDefinitionsTable);
+
+			for (int32_t i = 0; i < numTypes; i++)
+			{
+				uint32_t cols[MONO_TYPEDEF_SIZE];
+				mono_metadata_decode_row(typeDefinitionsTable, i, cols, MONO_TYPEDEF_SIZE);
+
+				const char* nameSpace = mono_metadata_string_heap(image, cols[MONO_TYPEDEF_NAMESPACE]);
+				const char* name = mono_metadata_string_heap(image, cols[MONO_TYPEDEF_NAME]);
+
+				printf("%s.%s\n", nameSpace, name);
+			}
+		}
+
+		
+
+	}
 	struct ScriptEngineData {
 		MonoDomain* RootDomain = nullptr;
 		MonoDomain* AppDomain = nullptr;
 
 		MonoAssembly* CoreAssembly = nullptr;
+		MonoImage* CoreAssemblyImage = nullptr;
+
+		ScriptClass EntityClass;
 	};
 
 	static ScriptEngineData* s_Data;
-
-	char* ReadBytes(const std::string& filepath, uint32_t* outSize)
-	{
-		std::ifstream stream(filepath, std::ios::binary | std::ios::ate);
-
-		if (!stream)
-		{
-			// Failed to open the file
-			return nullptr;
-		}
-
-		std::streampos end = stream.tellg();
-		stream.seekg(0, std::ios::beg);
-		uint32_t size = end - stream.tellg();
-
-		if (size == 0)
-		{
-			// File is empty
-			return nullptr;
-		}
-
-		char* buffer = new char[size];
-		stream.read((char*)buffer, size);
-		stream.close();
-
-		*outSize = size;
-		return buffer;
-	}
-
-	MonoAssembly* LoadCSharpAssembly(const std::string& assemblyPath)
-	{
-		uint32_t fileSize = 0;
-		char* fileData = ReadBytes(assemblyPath, &fileSize);
-
-		// NOTE: We can't use this image for anything other than loading the assembly because this image doesn't have a reference to the assembly
-		MonoImageOpenStatus status;
-		MonoImage* image = mono_image_open_from_data_full(fileData, fileSize, 1, &status, 0);
-
-		if (status != MONO_IMAGE_OK)
-		{
-			const char* errorMessage = mono_image_strerror(status);
-			// Log some error message using the errorMessage data
-			return nullptr;
-		}
-
-		MonoAssembly* assembly = mono_assembly_load_from_full(image, assemblyPath.c_str(), &status, 0);
-		mono_image_close(image);
-
-		// Don't forget to free the file data
-		delete[] fileData;
-
-		return assembly;
-	}
-
-	void PrintAssemblyTypes(MonoAssembly* assembly)
-	{
-		MonoImage* image = mono_assembly_get_image(assembly);
-		const MonoTableInfo* typeDefinitionsTable = mono_image_get_table_info(image, MONO_TABLE_TYPEDEF);
-		int32_t numTypes = mono_table_info_get_rows(typeDefinitionsTable);
-
-		for (int32_t i = 0; i < numTypes; i++)
-		{
-			uint32_t cols[MONO_TYPEDEF_SIZE];
-			mono_metadata_decode_row(typeDefinitionsTable, i, cols, MONO_TYPEDEF_SIZE);
-
-			const char* nameSpace = mono_metadata_string_heap(image, cols[MONO_TYPEDEF_NAMESPACE]);
-			const char* name = mono_metadata_string_heap(image, cols[MONO_TYPEDEF_NAME]);
-
-			printf("%s.%s\n", nameSpace, name);
-		}
-	}
-
 
 	void ScriptEngine::Init()
 	{
 		s_Data = new ScriptEngineData();
 		InitMono();
+		LoadAssembly("Vectora-ScriptCore.dll");
+		ScriptGlue::RegisterFunctions();
 
+		s_Data->EntityClass = ScriptClass("Vectora", "Entity");
+		MonoObject* instance = s_Data->EntityClass.Instantiate();
+
+		int value = 5;
+		void* param = &value;
+		void* args[] = { &value };
+		MonoMethod* printMessageInt = s_Data->EntityClass.GetMethod("PrintCustomInt", 1);
+		s_Data->EntityClass.InvokeMethod(instance, printMessageInt, args);
+
+		MonoString* monoString = mono_string_new(s_Data->AppDomain, "Hello from C++");
+		void* stringArgs[] = { monoString };
+		MonoMethod* printCustomMessage = s_Data->EntityClass.GetMethod("PrintCustomMessage", 1);
+		s_Data->EntityClass.InvokeMethod(instance, printCustomMessage, stringArgs);
 	}
 	void ScriptEngine::Shutdown()
 	{
 		ShutdownMono();
 		delete s_Data;
-	}
-
-	static void NativeLog(MonoString* text, int param) {
-		char* cStr = mono_string_to_utf8(text);
-		std::string str(cStr);
-		mono_free(cStr);
-		std::cout << str << ", " << param << "\n";
-	}
-
-	static void NativeLogV3(glm::vec3* parameter) {
-		//VE_CORE_WARN("Value: {0}", *parameter);
-		glm::vec3 ran(1.f, 2.f, 3.f);
-		VE_WARN("{0}", ran);
 	}
 
 	void ScriptEngine::InitMono()
@@ -123,33 +129,6 @@ namespace Vectora {
 
 		// Store the root domain pointer
 		s_Data->RootDomain = rootDomain;
-		std::string tt = "VectoraAppDomain";
-		s_Data->AppDomain = mono_domain_create_appdomain(const_cast<char*>("VectoraScriptRuntime"), nullptr);
-		mono_domain_set(s_Data->AppDomain, true);
-
-		mono_add_internal_call("Vectora.Main::NativeLog", NativeLog);
-		mono_add_internal_call("Vectora.Main::NativeLog_Vector", NativeLogV3);
-
-		s_Data->CoreAssembly = LoadCSharpAssembly("Vectora-ScriptCore.dll");
-		PrintAssemblyTypes(s_Data->CoreAssembly);
-
-		MonoImage* assemblyImage = mono_assembly_get_image(s_Data->CoreAssembly);
-		MonoClass* monoClass = mono_class_from_name(assemblyImage, "Vectora", "Main");
-
-		MonoObject* instance = mono_object_new(s_Data->AppDomain, monoClass);
-		mono_runtime_object_init(instance);
-
-
-		int value = 5;
-		void* param = &value;
-		void* args[] = { &value };
-		MonoMethod* printMessageInt = mono_class_get_method_from_name(monoClass, "PrintCustomInt", 1);
-		mono_runtime_invoke(printMessageInt, instance, args, nullptr);
-
-		MonoString* monoString = mono_string_new(s_Data->AppDomain, "Hello from C++");
-		void* stringArgs[] = { monoString };
-		MonoMethod* printCustomMessage = mono_class_get_method_from_name(monoClass, "PrintCustomMessage", 1);
-		mono_runtime_invoke(printCustomMessage, instance, stringArgs, nullptr);
 	}
 	void ScriptEngine::ShutdownMono()
 	{
@@ -157,5 +136,42 @@ namespace Vectora {
 		s_Data->AppDomain = nullptr;
 		//mono_jit_cleanup(s_Data->RootDomain);
 		s_Data->RootDomain = nullptr;
+	}
+	void ScriptEngine::LoadAssembly(const std::filesystem::path& filePath)
+	{
+		s_Data->AppDomain = mono_domain_create_appdomain(const_cast<char*>("VectoraScriptRuntime"), nullptr);
+		mono_domain_set(s_Data->AppDomain, true);
+
+
+		s_Data->CoreAssembly = Utils::LoadMonoAssembly(filePath);
+		s_Data->CoreAssemblyImage = mono_assembly_get_image(s_Data->CoreAssembly);
+	}
+
+	MonoObject* ScriptEngine::InstantiateClass(MonoClass* monoClass)
+	{
+		MonoObject* instance = mono_object_new(s_Data->AppDomain, monoClass);
+		mono_runtime_object_init(instance);
+		return instance;
+	};
+
+	ScriptClass::ScriptClass(const std::string& classNamespace, const std::string& className)
+		: m_ClassNamespace(classNamespace), m_ClassName(className)
+	{
+		m_MonoClass = mono_class_from_name(s_Data->CoreAssemblyImage, classNamespace.c_str(), className.c_str());
+	}
+
+	MonoObject* ScriptClass::Instantiate()
+	{
+		return ScriptEngine::InstantiateClass(m_MonoClass);
+	}
+
+	MonoMethod* ScriptClass::GetMethod(const std::string& name, int parameterCount)
+	{
+		return mono_class_get_method_from_name(m_MonoClass, name.c_str(), parameterCount);
+	}
+
+	MonoObject* ScriptClass::InvokeMethod(MonoObject* instance, MonoMethod* method, void** params)
+	{
+		return mono_runtime_invoke(method, instance, params, nullptr);
 	}
 }
