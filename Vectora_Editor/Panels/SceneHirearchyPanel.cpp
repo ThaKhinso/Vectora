6c2053e08233a3958820d6f046cccbe3d3b48d45
soethiha <usersoe123@gmail.com>
2026-05-01 10:29:27 +0630

    engine reading public files of scripts

--- Vectora_Editor/Panels/SceneHirearchyPanel.cpp
+++ Vectora_Editor/Panels/SceneHirearchyPanel.cpp
@@ -315,7 +315,7 @@ namespace Vectora {
 				}
 		});
 
-		DrawComponent<ScriptComponent>("Script", entity, [](auto& component)
+		DrawComponent<ScriptComponent>("Script", entity, [entity](auto& component) mutable
 		{
 			bool scriptClassExists = ScriptEngine::EntityClassExists(component.ClassName);
 
@@ -327,6 +327,24 @@ namespace Vectora {
 
 			if (ImGui::InputText("Class", buffer, sizeof(buffer)))
 				component.ClassName = buffer;
+
+			Ref<ScriptInstance> scriptInstance = ScriptEngine::GetEntityScriptInstance(entity.GetUUID());
+			if (scriptInstance)
+			{
+				const auto& fields = scriptInstance->GetScriptClass()->GetFields();
+
+				for (const auto& [name, field] : fields)
+				{
+					if (field.Type == ScriptFieldType::Float)
+					{
+						float data = scriptInstance->GetFieldValue<float>(name);
+						if (ImGui::DragFloat(name.c_str(), &data))
+						{
+							scriptInstance->SetFieldValue(name, data);
+						}
+					}
+				}
+			}
 
 			if (!scriptClassExists)
 				ImGui::PopStyleColor();
--- Vectora_Engine/Scripting/ScriptEngine.h
+++ Vectora_Engine/Scripting/ScriptEngine.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <filesystem>
+#include <map>
+
 #include "Scene/Entity.h"
 #include "Scene/Scene.hpp"
 
@@ -10,9 +12,28 @@ extern "C" {
 	typedef struct _MonoMethod MonoMethod;
 	typedef struct _MonoAssembly MonoAssembly;
 	typedef struct _MonoImage MonoImage;
+	typedef struct _MonoClassField MonoClassField;
 }
 
 namespace Vectora {
+	enum class ScriptFieldType
+	{
+		None = 0,
+		Float, Double,
+		Bool, Char, Byte, Short, Int, Long,
+		UByte, UShort, UInt, ULong,
+		Vector2, Vector3, Vector4,
+		Entity
+	};
+
+	struct ScriptField
+	{
+		ScriptFieldType Type;
+		std::string Name;
+
+		MonoClassField* ClassField;
+	};
+
 	class ScriptClass
 	{
 	public:
@@ -22,11 +43,16 @@ namespace Vectora {
 		MonoObject* Instantiate();
 		MonoMethod* GetMethod(const std::string& name, int parameterCount);
 		MonoObject* InvokeMethod(MonoObject* instance, MonoMethod* method, void** params = nullptr);
+
+		const std::map<std::string, ScriptField>& GetFields() const { return m_Fields; }
 	private:
 		std::string m_ClassNamespace;
 		std::string m_ClassName;
 
+		std::map<std::string, ScriptField> m_Fields;
 		MonoClass* m_MonoClass = nullptr;
+
+		friend class ScriptEngine;
 	};
 
 	class ScriptInstance
@@ -37,6 +63,27 @@ namespace Vectora {
 
 		void InvokeOnCreate();
 		void InvokeOnUpdate(float ts);
+
+		Ref<ScriptClass> GetScriptClass() { return m_ScriptClass; }
+
+		template<typename T>
+		T GetFieldValue(const std::string& name)
+		{
+			bool success = GetFieldValueInternal(name, s_FieldValueBuffer);
+			if (!success)
+				return T();
+
+			return *(T*)s_FieldValueBuffer;
+		}
+
+		template<typename T>
+		void SetFieldValue(const std::string& name, const T& value)
+		{
+			SetFieldValueInternal(name, &value);
+		}
+	private:
+		bool GetFieldValueInternal(const std::string& name, void* buffer);
+		bool SetFieldValueInternal(const std::string& name, const	void* value);
 	private:
 		Ref<ScriptClass> m_ScriptClass;
 
@@ -44,6 +91,8 @@ namespace Vectora {
 		MonoMethod* m_Constructor = nullptr;
 		MonoMethod* m_OnCreateMethod = nullptr;
 		MonoMethod* m_OnUpdateMethod = nullptr;
+
+		inline static char s_FieldValueBuffer[8];
 	};
 
 	class ScriptEngine {
@@ -61,6 +110,8 @@ namespace Vectora {
 
 		static MonoImage* GetCoreAssemblyImage();
 		static Scene* GetSceneContext();
+
+		static Ref<ScriptInstance> GetEntityScriptInstance(UUID entityID);
 
 		static bool EntityClassExists(const std::string& fullClassName) ;
 		static std::unordered_map<std::string, Ref<ScriptClass>> GetEntityClasses();
